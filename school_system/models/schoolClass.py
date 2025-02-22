b204ea79ea5d09648be661ee12e7de08a77694af
Selim <selim.reza.dia@gmail.com>
2025-02-22 19:15:01 +0600

    successfully established one2many connection between class and subjects

--- school_system/models/schoolClass.py
+++ school_system/models/schoolClass.py
@@ -6,3 +6,16 @@ class SchoolClass(models.Model):
 
     name = fields.Char("Class name")
     capacity = fields.Integer("Max students")
+    class_teacher = fields.Many2one('school.teacher', string="Class Teacher")
+    subject_line_ids = fields.One2many('class.subject.line', 'classroom_id', string="Subjects")
+
+
+class ClassSubjectLine(models.Model):
+    _name = 'class.subject.line'
+    _description = 'Class subject line'
+    _rec_name = 'subject_id'
+
+    subject_id = fields.Many2one('school.subjects', string="Select subject")
+    teacher_id = fields.Many2one('school.teacher', string="Select teacher")
+    schedule = fields.Char(string="Class time")
+    classroom_id = fields.Many2one('school.schoolclass', string="Classroom")
