e223569de036e89052dfb267c28c67226f6e1c1a
Selim <selim.reza.dia@gmail.com>
2025-03-15 14:28:29 +0600

    Add reports folder in custom_sales
    - Add reports folder
    
    - add inherit xml file
    
    - link that xml file to manifest file

--- custom_sales/__manifest__.py
+++ custom_sales/__manifest__.py
@@ -4,6 +4,7 @@
     'author': 'Selim Reza',
     'website': 'www.example.com',
     'data': [
+        'reports/sale_order_template_inherit.xml',
         'views/inherited_sale_order_form.xml',
     ],
     'depends': ['sale','sale_management'],
