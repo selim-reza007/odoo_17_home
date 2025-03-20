411c1bb36cae84bdb22b5fccd74f9fc0e5f96469
Selim <selim.reza.dia@gmail.com>
2025-03-20 16:50:15 +0600

    Add constraint logic to tag model for unique name

--- doctor_management/models/tags.py
+++ doctor_management/models/tags.py
@@ -4,3 +4,7 @@ class Tags(models.Model):
     _name = "hospital.tags"
 
     name = fields.Char("Tag name")
+
+    _sql_constraints = [
+        ('unique_tag_name', "unique(name)", "Tag name must be unique!")
+    ]
