4a3ba8d7c7a4b10f2eec705eb0d98034eb0437cd
Selim <selim.reza.dia@gmail.com>
2025-03-20 16:12:02 +0600

    Add domain logic when fetching appointment record to cancel wizard

--- doctor_management/wizard/cancel_appointment.py
+++ doctor_management/wizard/cancel_appointment.py
@@ -4,7 +4,7 @@ class CancelWizard(models.TransientModel):
     _name = "hospital.cancel.appointment.wizard"
     _description = "hospital cancel appointment wizard"
 
-    ref = fields.Many2one('hospital.appointment', string="ref no.")
+    ref = fields.Many2one('hospital.appointment', string="ref no.", domain=[('state','=','bed')])
     patient_id = fields.Many2one(related='ref.patient_id', string="Patient")
     cancel_date = fields.Date(string="Date", default=fields.Date.today())
     reason = fields.Char(string="Reason")
