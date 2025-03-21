fc6d4c8e5ddc9c7b4de94c3c415bb2559a9153c3
Selim <selim.reza.dia@gmail.com>
2025-03-21 11:54:43 +0600

    Add copy method to tags model class

--- doctor_management/models/tags.py
+++ doctor_management/models/tags.py
@@ -1,9 +1,18 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 class Tags(models.Model):
     _name = "hospital.tags"
 
     name = fields.Char("Tag name")
+    color = fields.Integer("Color")
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        if 'name' not in default:
+            default['name'] = _("%s (copy)", self.name)
+        return super(Tags, self).copy(default)
 
     _sql_constraints = [
         ('unique_tag_name', "unique(name)", "Tag name must be unique!")
