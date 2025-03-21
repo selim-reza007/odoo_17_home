1a29b6a96a2db51cb23d55b8d92eee6df6839ac2
Selim <selim.reza.dia@gmail.com>
2025-03-21 11:02:52 +0600

    Add dob in patient also add age calculation

--- doctor_management/models/patient.py
+++ doctor_management/models/patient.py
@@ -1,9 +1,21 @@
+import datetime
 from odoo import api, models, fields
+from odoo.odoo.tools.populate import compute
+
 
 class Patient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital patient'
 
     name = fields.Char("Name")
-    age = fields.Integer("Age")
+    age = fields.Integer(compute="_age_calculate",string="Age")
     mobile = fields.Char("Contact no.")
+    dob = fields.Date("Date of birth")
+
+    @api.onchange('dob')
+    def _age_calculate(self):
+        for rec in self:
+            if rec.dob:
+                rec.age = datetime.date.today().year - rec.dob.year
+            else:
+                rec.age = 1
