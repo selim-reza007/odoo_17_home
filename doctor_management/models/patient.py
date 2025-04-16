6e15026facb8368831f957111e98d7bc1cdb73e0
Selim <selim.reza.dia@gmail.com>
2025-04-16 19:40:49 +0600

    Add inverse method for calculation date based on years in patient

--- doctor_management/models/patient.py
+++ doctor_management/models/patient.py
@@ -1,18 +1,28 @@
 import datetime
+
+from dateutil.utils import today
+
 from odoo import api, models, fields,_
 from odoo.odoo.tools.populate import compute
 from odoo.exceptions import ValidationError
+from dateutil.relativedelta import relativedelta
 
 class Patient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital patient'
 
     name = fields.Char("Name")
-    age = fields.Integer(compute="_age_calculate",string="Age")
+    age = fields.Integer(compute="_age_calculate", inverse="inverse_age_calculate",string="Age")
     mobile = fields.Char("Contact no.")
     dob = fields.Date("Date of birth")
     appointment_count = fields.Integer(compute="_compute_appointment",string="Appointment no.", store=True)
     appointments_ids = fields.One2many('hospital.appointment','patient_id',string="appointment line")
+
+    @api.depends('age')
+    def inverse_age_calculate(self):
+        today = fields.Date.today()
+        for rec in self:
+            rec.dob = today - relativedelta(years=rec.age)
 
     @api.ondelete(at_uninstall=True)
     def _check_appointment(self):
