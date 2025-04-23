2d720eb5e3758584b39fd060bb42e543ba788604
Selim <selim.reza.dia@gmail.com>
2025-04-23 16:03:43 +0600

    Add widget handle to operation model

--- doctor_management/models/operation.py
+++ doctor_management/models/operation.py
@@ -4,10 +4,12 @@ class Operation(models.Model):
     _name = 'hospital.operation'
     _description = 'hospital operation'
     _log_access = False
+    _order = 'sequence, id'
 
     doctor_id = fields.Many2one("doctor.doctor", string="Doctor")
     operation_name = fields.Char("Name")
     reference_record = fields.Reference(selection=[('hospital.patient','Patient'), ('hospital.appointment','Appointment')], string='Record')
+    sequence = fields.Integer(string="Sequence", default=10)
 
     @api.model
     def name_create(self, name):
