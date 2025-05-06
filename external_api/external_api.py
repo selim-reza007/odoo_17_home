3cfc3d8639b3910dbfff8c9c4be4ef93be622506
Selim <selim.reza.dia@gmail.com>
2025-05-06 17:18:32 +0600

    Update external api code to get res.partner table data

--- external_api/external_api.py
+++ external_api/external_api.py
@@ -12,3 +12,16 @@ if uid:
     print("Authentication success!")
 else:
     print("Authentication fail!")
+
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+#search method
+partner = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]], {'offset': 2, 'limit': 5})
+# print("Partner : ",partner)
+
+#read method
+partner_record = models.execute_kw(db, uid, password, 'res.partner', 'read', [partner],  {'fields': ['id', 'name']})
+# print(partner_record)
+
+#search count
+partner_record_count = models.execute_kw(db, uid, password, 'res.partner', 'search_count', [[['is_company', '=', True]]])
+print("partner_record_count : ",partner_record_count)
